67a8d205389afb1a062f625e877b3ef4a4cd3caa
nancylukas <nlukas13@optonline.net>
2016-09-26 13:38:27 -0400

    php file update

--- app/components/Home.js
+++ app/components/Home.js
@@ -8,7 +8,7 @@ function Home () {
   return (
     <MainContainer>
       <h1>Github Battle</h1>
-      <p className='lead'>Are you a Github Mater or Newbie?</p>
+      <p className='lead'>Are you a Github Master or Newbie?</p>
       <Link to='/playerOne'>
         <button type='button' className='btn btn-lg btn-success'>Get Started</button>
       </Link>
